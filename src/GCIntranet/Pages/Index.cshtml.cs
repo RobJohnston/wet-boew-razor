4ccc5f9372fac95944afd0472fd9116d36849278
Rob Johnston <RobJohnston@users.noreply.github.com>
2019-12-12 20:12:38 -0500

    Localize page partials and add WET error pages.

--- src/GCIntranet/Pages/Index.cshtml.cs
+++ src/GCIntranet/Pages/Index.cshtml.cs
@@ -11,7 +11,11 @@ namespace GCIntranet.Pages
     {
         public void OnGet()
         {
+            // Test the 404 error page by loading any page that doesn't exist.
 
+            // Test the server message page by uncomment the following line and ensure
+            // the Configure method in Startup.cs doesn't use the developer exception page.
+            //throw new ApplicationException("Test error page.");
         }
     }
 }
